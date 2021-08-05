10c5d9f7808f3ec628d1f07ed1b11d77dcd6010b
Your Name <you@example.com>
2021-08-05 12:01:39 +0545

    Settings

--- server/configs/gunicorn.sh
+++ server/configs/gunicorn.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-NAME="portal"                              #Name of the application (*)
-DJANGODIR=/home/ppc/deploy/GandakiProvinceData    # Django project directory (*)
-SOCKFILE=/home/ppc/deploy/run/gunicorn.sock        # we will communicate using this unix socket (*)
-USER=ppc                                        # the user to run as (*)
-GROUP=ppc                                     # the group to run as (*)
+NAME="inventoryFEN"                              #Name of the application (*)
+DJANGODIR=/media/main_storage/anupadkh/inventoryCMS    # Django project directory (*)
+SOCKFILE=/media/main_storage/anupadkh/run/inventory.sock        # we will communicate using this unix socket (*)
+USER=anupadkh                                       # the user to run as (*)
+GROUP=anupadkh                                    # the group to run as (*)
 NUM_WORKERS=1                                     # how many worker processes should Gunicorn spawn (*)
-DJANGO_SETTINGS_MODULE=configs.prod             # which settings file should Django use (*)
-DJANGO_WSGI_MODULE=configs.wsgi                     # WSGI module name (*)
-VENV_DIR=/home/ppc/deploy/portal_venv                      #VirtualEnv directory
+DJANGO_SETTINGS_MODULE=fEN.settings             # which settings file should Django use (*)
+DJANGO_WSGI_MODULE=fEN.wsgi                     # WSGI module name (*)
+VENV_DIR=/media/main_storage/anupadkh/fEN                      #VirtualEnv directory
 
 echo "Starting $NAME as `whoami`"
 
 # Activate the virtual environment
-source /home/ppc/.bash_profile
+# source /home/ppc/.bash_profile
 cd $DJANGODIR
 source $VENV_DIR/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
